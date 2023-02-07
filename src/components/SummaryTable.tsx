cbc07f73d0171f15bd6581f0d7848fbc129afe76
Matchobas <maalexandria@hotmail.com>
2023-02-06 23:15:24 -0300

    Creating the summary table

---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,25 @@
+const weekDays = [
+  'D',
+  'S',
+  'T',
+  'Q',
+  'Q',
+  'S',
+  'S'
+]
+
+export function SummaryTable() {
+  return (
+    <div className='w-full flex'>
+      <div className='grid grid-rows-7 grid-flow-row gap-3'>
+        {weekDays.map((weekDay) => {
+          return (
+            <div className='text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center'>
+              {weekDay}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
