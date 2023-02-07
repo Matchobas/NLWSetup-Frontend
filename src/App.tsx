10ad9fbc11a4e7683fce7d38d96284d86441e7a4
Matchobas <maalexandria@hotmail.com>
2023-02-06 22:45:34 -0300

    First step in the main screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,12 @@ import './styles/global.css';
 
 import { Habit } from "./components/Habit";
 
-function App() {
+export function App() {
   return (
-    <>
-      <Habit completed={1} />
-      <Habit completed={2} />
-      <Habit completed={3} />
-      <Habit completed={4} />
-      <Habit completed={5} />
-      <Habit completed={6} />
-    </>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex-col gap-16'>
+
+      </div>
+    </div>
   )
 }
-
-export default App
