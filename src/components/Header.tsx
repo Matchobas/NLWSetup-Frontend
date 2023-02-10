998f9624695eb3593f9b66b141341b85c952afa6
Matchobas <maalexandria@hotmail.com>
2023-02-09 22:46:15 -0300

    Creating new habit modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Plus } from 'phosphor-react';
+import { useState } from 'react';
+import * as Dialog from "@radix-ui/react-dialog";
 
 import logoImage from '../assets/logo.svg';
 
@@ -6,13 +8,19 @@ export function Header() {
   return (
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
       <img src={logoImage} alt='Habits logo  ' />
-      <button 
-        type='button'
-        className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
-      >
-        <Plus size={20} className='text-violet-500' />
-        Novo hábito
-      </button>
+      <Dialog.Root>
+        <Dialog.Trigger
+          type='button'
+          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+        >
+          <Plus size={20} className='text-violet-500' />
+          Novo hábito
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   )
 }
