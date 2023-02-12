720afd6be429b0bf78fdf02836b8157f69aa6418
Matchobas <maalexandria@hotmail.com>
2023-02-11 23:50:33 -0300

    Dinamic percentage bar with props

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,7 +1,14 @@
 import * as Popover from "@radix-ui/react-popover";
 import { ProgressBar } from "./ProgressBar";
 
-export function Habit() {
+interface HabitProps {
+  amount: number;
+  completed: number;
+}
+
+export function Habit({ completed, amount}: HabitProps) {
+  const completedPercentage = Math.round((completed / amount) * 100);
+
   return (
     <Popover.Root>
       <Popover.Trigger className='w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg' />
@@ -11,7 +18,7 @@ export function Habit() {
           <span className="font-semibold text-zinc-400">Sexta-feira</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">10/02</span>
 
-          <ProgressBar progress={45} />
+          <ProgressBar progress={completedPercentage} />
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
