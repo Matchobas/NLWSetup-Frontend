2fb29819bd15182c56d7041550b852863e662567
Matchobas <maalexandria@hotmail.com>
2023-02-11 23:44:40 -0300

    Progress bar component created

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,4 +1,5 @@
 import * as Popover from "@radix-ui/react-popover";
+import { ProgressBar } from "./ProgressBar";
 
 export function Habit() {
   return (
@@ -9,6 +10,8 @@ export function Habit() {
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
           <span className="font-semibold text-zinc-400">Sexta-feira</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">10/02</span>
+
+          <ProgressBar progress={45} />
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
