8fd3bacbe813f1e7a85fe1d93eaf6ecda168abe6
Matchobas <maalexandria@hotmail.com>
2023-02-13 00:22:02 -0300

    Create checkbox styles

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -31,7 +31,13 @@ export function SummaryTable() {
 
       <div className='grid grid-rows-7 grid-flow-col gap-3'>
         {summaryDates.map((date) => {
-          return <Habit amount={5} completed={3} key={date.toString()} />
+          return (
+            <Habit 
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          )
         })}
 
         {amountsOfDaysToFill > 0 && Array.from({ length: amountsOfDaysToFill }).map((_, i) => {
